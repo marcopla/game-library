0a091276cc060f4e9266375e512ff46b586810f4
Marco <marco.pla@gmail.com>
2023-01-24 17:51:48 -0300

    for and endfor

--- jogoteca.py
+++ jogoteca.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html', titulo='Jogos') 
+    lista_de_jogos = ['Tetris','Skyrim','Crash']
+    return render_template('lista.html', titulo = 'Jogos', jogos = lista_de_jogos) 
 
 app.run(host='0.0.0.0', port=8080)
