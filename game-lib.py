ea2066f9ea13540fb56d3e67ec41f35b499a53f7
Marco <marco.pla@gmail.com>
2023-01-27 15:22:40 -0300

    POST method and debug property added

--- game-lib.py
+++ game-lib.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,20 +9,27 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack and Slash', 'PS2')
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Jogo('God of War', 'Rack and Slash', 'PS2')
+lista_de_jogos = [jogo1, jogo2]
 
-    lista_de_jogos = [jogo1, jogo2]
-
-    return render_template('lista.html', titulo='Jogos', jogos=lista_de_jogos)
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos = lista_de_jogos)
 
 
 @app.route('/novo')
 def novo():
-    return render_template('novo.html', titulo='Novo Jogo', )
+    return render_template('novo.html', titulo ='Novo Jogo', )
 
+@app.route('/criar', methods = ['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista_de_jogos.append(jogo)
+    return render_template('lista.html', titulo = 'Jogos', jogos = lista_de_jogos)
 
-app.run(host='0.0.0.0', port=8080)
+app.run(host = '0.0.0.0', port = 8080, debug = True)
