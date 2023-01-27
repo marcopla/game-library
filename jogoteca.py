7252955339b94c452bf2b217b1df5c171849ad5f
Marco Pla <marco.pla@gmail.com>
2023-01-27 00:08:55 -0300

    OOP

--- jogoteca.py
+++ jogoteca.py
@@ -2,9 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 @app.route('/inicio')
 def ola():
-    lista_de_jogos = ['Tetris','Skyrim','Crash']
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('God of War', 'Rack and Slash', 'PS2')
+
+    lista_de_jogos = [jogo1, jogo2]
+    
     return render_template('lista.html', titulo = 'Jogos', jogos = lista_de_jogos) 
 
 app.run(host='0.0.0.0', port=8080)
