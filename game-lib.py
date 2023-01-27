991a0bd7310a225acfef6cbbb16dcd4efc81cbc2
Marco Pla <marco.pla@gmail.com>
2023-01-27 00:26:08 -0300

    new route "novo"

--- game-lib.py
+++ game-lib.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 class Jogo:
     def __init__(self, nome, categoria, console):
         self.nome = nome
         self.categoria = categoria
         self.console = console
+
 
 @app.route('/inicio')
 def ola():
@@ -14,7 +16,13 @@ def ola():
     jogo2 = Jogo('God of War', 'Rack and Slash', 'PS2')
 
     lista_de_jogos = [jogo1, jogo2]
-    
-    return render_template('lista.html', titulo = 'Jogos', jogos = lista_de_jogos) 
+
+    return render_template('lista.html', titulo='Jogos', jogos=lista_de_jogos)
+
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo', )
+
 
 app.run(host='0.0.0.0', port=8080)
