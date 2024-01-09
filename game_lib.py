87cc16a32bbfb61914de0a553b2de62b2ffa5609
Marco Pla <marco.pla@gmail.com>
2024-01-09 18:44:18 -0300

    fix(lista) : delete route removed until feature is completed

--- game_lib.py
+++ game_lib.py
@@ -9,4 +9,4 @@ db = SQLAlchemy(app)
 from views import *
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 8080, debug = True)
+    app.run(host = '0.0.0.0', port = 8082, debug = True)
